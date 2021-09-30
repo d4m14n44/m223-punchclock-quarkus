fe097fc56e97030664204d16c4d611905fc34cd9
Damian <damian.oberholzer1234@gmail.com>
2021-09-30 08:40:53 +0200

    CategoryService implementiert

--- src/main/java/ch/zli/m223/punchclock/service/CategoryService.java
+++ src/main/java/ch/zli/m223/punchclock/service/CategoryService.java
@@ -1,5 +1,50 @@
 package ch.zli.m223.punchclock.service;
 
+import ch.zli.m223.punchclock.domain.Category;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
 // Gleiche Methoden einfach mit anderen Daten implementieren
+@ApplicationScoped
 public class CategoryService {
+
+    @Inject
+    private EntityManager entityManager;
+
+    @Transactional
+    public Category createCategory(Category category) {
+        this.entityManager.persist(category);
+        return category;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Category> findAll() {
+        var query = this.entityManager.createQuery("FROM Category");
+        return query.getResultList();
+    }
+
+    public Category getCategory(Long Id) {
+        return this.entityManager.find(Category.class, Id);
+    }
+
+    @Transactional
+    public void delete(Long Id) {
+        Category category = getCategory(Id);
+        this.entityManager.remove(category);
+    }
+
+    @Transactional
+    public Category update(Category category) {
+        this.entityManager.merge(category);
+        return category;
+    }
+
+    public EntityManager getEntityManager() {
+        return this.entityManager;
+    }
+
 }
