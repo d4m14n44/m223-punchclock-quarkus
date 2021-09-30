cc9cee73e5c68558bec22fc8617ab2659b62986a
Damian <damian.oberholzer1234@gmail.com>
2021-09-30 08:46:04 +0200

    UserController implementiert

--- src/main/java/ch/zli/m223/punchclock/controller/UserController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/UserController.java
@@ -1,4 +1,56 @@
 package ch.zli.m223.punchclock.controller;
-// Gleiche Methoden einfach mit anderen Daten implementieren
+
+import ch.zli.m223.punchclock.domain.User;
+import ch.zli.m223.punchclock.service.UserService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+import java.util.List;
+
+@Path("/users")
+@Tag(name = "Users", description = "Handling of users")
 public class UserController {
+
+    @Inject
+    UserService userService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "List all Users", description = "")
+    public List<User> list() {
+        return this.userService.findAll();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{user}")
+    public User getUser(@PathParam("user") Long Id) {return this.userService.getUser(Id);}
+
+    @DELETE
+    @Path("/{user}")
+    public void delete(@PathParam("user") Long Id) {this.userService.delete(Id);}
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public User updateUser(User user) {
+        return this.userService.update(user);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Add a new User", description = "The newly created user is returned. The ID may not be passed")
+    public User add(User user) {
+        return this.userService.createUser(user);
+    }
+
+    @DELETE
+    public void delete(User user) {this.userService.findAll().remove(user);}
+
 }
