2916877d68357aa6aa0b96ee1713d0f191c62cbd
Damian <damian.oberholzer1234@gmail.com>
2021-09-30 11:04:35 +0200

    Standard Location ins SQL Skript hinzugefügt

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,4 @@ INSERT INTO category (title) VALUES ('Admin');
 INSERT INTO category (title) VALUES ('Projekt');
 INSERT INTO category (title) VALUES ('IT-Support');
 INSERT INTO user (username, password) VALUES ('Test', 'test123');
+INSERT INTO location (name) VALUES ('Zuerich');
