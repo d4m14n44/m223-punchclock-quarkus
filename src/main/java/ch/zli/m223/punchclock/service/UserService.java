e278f5788633558748f4222c148acbb11095d29c
Damian <damian.oberholzer1234@gmail.com>
2021-09-30 08:45:01 +0200

    UserService implementiert

--- src/main/java/ch/zli/m223/punchclock/service/UserService.java
+++ src/main/java/ch/zli/m223/punchclock/service/UserService.java
@@ -1,4 +1,49 @@
 package ch.zli.m223.punchclock.service;
-// Gleiche Methoden einfach mit anderen Daten implementieren
+
+import ch.zli.m223.punchclock.domain.User;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@ApplicationScoped
 public class UserService {
+
+    @Inject
+    private EntityManager entityManager;
+
+    @Transactional
+    public User createUser(User user) {
+        this.entityManager.persist(user);
+        return user;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<User> findAll() {
+        var query = this.entityManager.createQuery("FROM User");
+        return query.getResultList();
+    }
+
+    public User getUser(Long Id) {
+        return this.entityManager.find(User.class, Id);
+    }
+
+    @Transactional
+    public void delete(Long Id) {
+        User user = getUser(Id);
+        this.entityManager.remove(user);
+    }
+
+    @Transactional
+    public User update(User user) {
+        this.entityManager.merge(user);
+        return user;
+    }
+
+    public EntityManager getEntityManager() {
+        return this.entityManager;
+    }
+
 }
